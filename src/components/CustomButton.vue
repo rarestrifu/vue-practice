75a126f93c3c8718f07898755ad2a273a6167ed5
rarestrifu <rarestrifu2001@yahoo.com>
2023-07-13 15:21:12 +0300

    ff

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 const buttonLabel = ref("Click me");
 const buttonId = "btn1";
-const text = ref("this is a text");
+const text = computed(() => isTextDisplayed.value ? "Text Displayed" : "Text not displayed");
 const isTextDisplayed = ref(true);
 const isButtonDisabled = false;
 
@@ -22,13 +22,20 @@ function clickHandler() {
         <button :id="`buttonId-${buttonId}`" :disabled="isButtonDisabled" v-on:click="clickHandler">
             {{ buttonLabel }}
         </button>
-        <span v-if="isTextDisplayed">
-            Text Displayed
-        </span>
-        <span v-else>
-            Text not displayed
+        <span>{{ isTextDisplayed }}</span>
+        <span>
+            {{ text }}
         </span>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    > * {
+        margin-left: 10px;
+    }
+}
+</style>
