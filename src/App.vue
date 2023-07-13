fedc08bf6fb06d958a426f52870801e01f9c9761
rarestrifu <rarestrifu2001@yahoo.com>
2023-07-13 15:10:44 +0300

    vue practice

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CustomButton from './components/CustomButton.vue';
+</script>
+
+<template>
+  <CustomButton/>
+</template>
+
+<style>
+body {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const buttonLabel = ref("Click me");
+const buttonId = "btn1";
+const text = ref("this is a text");
+const isTextDisplayed = ref(true);
+const isButtonDisabled = false;
+
+const reactiveObj = reactive({
+    title: 'reactive object'
+});
+
+function clickHandler() {
+    isTextDisplayed.value = !isTextDisplayed.value;
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <button :id="`buttonId-${buttonId}`" :disabled="isButtonDisabled" v-on:click="clickHandler">
+            {{ buttonLabel }}
+        </button>
+        <span v-if="isTextDisplayed">
+            Text Displayed
+        </span>
+        <span v-else>
+            Text not displayed
+        </span>
+    </div>
+</template>
+
+<style scoped></style>
